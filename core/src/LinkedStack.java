4395fbb9d18781a696c48b29abed031c61bab055
CCWindy <843905832@qq.com>
2016-02-24 19:27:49 +0800

    completed LinkedStack.java

--- core/src/LinkedStack.java
+++ core/src/LinkedStack.java
@@ -4,34 +4,61 @@ import java.util.Iterator;
  * {@code Stack}-like structure, implemented by using a linked list.
  */
 public class LinkedStack<E> implements Stack<E> {
-
+    private Node first = NULL;
+    private class Node{
+        Node next;
+        E item;
+    }
     @Override
     public void push(E item) {
         // TODO to be implemented
+        Node current = new Node();
+        current.next = first;
+        current.item = item;
+        first = current;
     }
 
     @Override
     public E pop() {
         // TODO to be implemented
-        return null;
+        item = first.item;
+        first = first.next;
+        return item;
     }
 
     @Override
     public E peek() {
         // TODO to be implemented
-        return null;
+        return first.item;
     }
 
     @Override
     public Iterator<E> iterator() {
         // TODO to be implemented
-        return null;
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<E>{
+        private Node first;
+        public boolean hasNext() {
+            return first != NULL;
+        }
+        public void remove () {
+            throw UnsupportedOperationException;    /* not supported */
+        }
+        public E next(){
+            if (!hasNext())
+                throw NoSuchElementException;
+            item = first.item;
+            first = first.next;
+            return item;
+        }
     }
 
     @Override
     public boolean isEmpty() {
         // TODO to be implemented
-        return false;
+        return first == NULL;
     }
 
 }
